1a8ba0e011e7eaa77d4d25f11326cd1d59ab2155
haadee <haadeescott@yahoo.com>
2021-03-21 03:47:31 +0800

    update subscriber

--- subscriber.py
+++ subscriber.py
@@ -18,10 +18,17 @@ client = TBDeviceMqttClient(THINGSBOARD_HOST, ACCESS_TOKEN)
 #Call back 
 def on_attribute_change(client,result, exception):
     print(client)
+
+    # get client key & values and print it 
+    # junction1_result will be the value
+    clientObj = result["client"]
+    junction1_result = clientObj["junction1"]
+    print(junction1_result)
     if exception is not None:
         print("Exception: " + str(exception))
     else:
         print(result)
+        
 
 client.connect()
 #client.request_attributes(["junction1", "junction2"], callback=on_attribute_change)
@@ -30,3 +37,6 @@ client.connect()
 while True:
     client.request_attributes(["Edge1_Len", "Edge1_Straight", "Edge1_Right", "Edge2_Len", "Edge2_Straight", "Edge2_Right"],callback=on_attribute_change)  
     time.sleep(5)
+
+
+
