18756fa819c80c6324411ead8501f5f01a20f507
AdamLoh <adamloh94@gmail.com>
2021-04-02 20:22:34 +0800

    Mock data

---
+++ data.py
@@ -0,0 +1,86 @@
+data = [
+    {
+        section: 1,
+        lanes: [
+            {
+                lane: 1,
+                cars: 3
+            },
+            {
+                lane: 2,
+                cars: 3
+            },
+            {
+                lane: 3,
+                cars: 3
+            },
+            {
+                lane: 4,
+                cars: 3
+            }
+        ]
+    },
+    {
+        section: 2,
+        lanes: [
+            {
+                lane: 1,
+                cars: 3
+            },
+            {
+                lane: 2,
+                cars: 3
+            },
+            {
+                lane: 3,
+                cars: 3
+            },
+            {
+                lane: 4,
+                cars: 3
+            }
+        ]
+    },
+    {
+        section: 3,
+        lanes: [
+            {
+                lane: 1,
+                cars: 3
+            },
+            {
+                lane: 2,
+                cars: 3
+            },
+            {
+                lane: 3,
+                cars: 3
+            },
+            {
+                lane: 4,
+                cars: 3
+            }
+        ]
+    },
+    {
+        section: 4,
+        lanes: [
+            {
+                lane: 1,
+                cars: 3
+            },
+            {
+                lane: 2,
+                cars: 3
+            },
+            {
+                lane: 3,
+                cars: 3
+            },
+            {
+                lane: 4,
+                cars: 3
+            }
+        ]
+    }
+]
