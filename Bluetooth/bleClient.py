1ef3e48cbc19403bc1f549bf8400d07aa76cf131
haadee <haadeescott@yahoo.com>
2021-04-04 02:10:00 +0800

    updated BLE client to record time and append in txt file

---
+++ Bluetooth/bleClient.py
@@ -0,0 +1,24 @@
+from bluepy import btle
+import time
+
+SYSTEM_TIME = time.time()
+
+def writeTxt(inputTime):
+    time = str(inputTime) + "\r\n"
+    f = open("Controller_Bluetooth.txt", "a")
+    f.write(time)
+    f.close()
+
+def getCurrentTime(input):
+    timenow = str(round((time.time()-SYSTEM_TIME)*1000)) + " ms"
+    writeTxt(timenow)
+
+
+print("Connecting...")
+dev = btle.Peripheral("b8:27:eb:82:de:18")
+
+print("Services...")
+for svc in dev.services:
+    for char in svc.getCharacteristics():
+        getCurrentTime(print(char.read()))
+        
